e36d20baf8f41536f7c3e42ab852a202790dae71
Dip Chowdhury <dipchry@gmail.com>
2026-04-19 23:44:46 +0200

    wip(script): bwa+bowtie2 comparison for polishing

---
+++ code/05b_align_polish_canu_pacbio_with_illumina_bowtie2.sh
@@ -0,0 +1,21 @@
+# TO DO
+
+#!/bin/bash
+#SBATCH -A uppmax2026-1-61
+#SBATCH -p pelle
+#SBATCH -c 2
+#SBATCH -t 04:00:00
+#SBATCH -J align_polish_canu_pacbio_with_illumina_bowtie2
+#SBATCH --mail-type=ALL
+#SBATCH --output=/home/dich3309/rnaseq-tnseq-enterococcus-analysis/log/05b_align_polish_canu_pacbio_with_illumina_bowtie2.%x.%j.out
+
+source "${HOME}/rnaseq-tnseq-enterococcus-analysis/utils/paths.sh"
+
+# all outputs go to nobackup (bam, index, flagstat); symlink into analyses/
+mkdir -p "${ASSEMBLY_DIR}" "${NOBACKUP_POLISH}"
+ensure_nobackup_symlink "${POLISH_ALIGN_DIR}" "${NOBACKUP_POLISH}"
+
+module purge
+module load Bowtie2/2.5.4-GCC-13.3.0
+module load SAMtools/1.22.1-GCC-13.3.0
+
